90d40ec8bad2fd9d7928c75eba585367c4f8f8f5
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-15 21:08:04 +0300

    Build

--- frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer className="footer">
-      <p className="footer__copy">{`© ${new Date().getFullYear()}. Руслан Газиев`}</p>
-    </footer>
-  );
-}
-
-export default Footer;
