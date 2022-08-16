98591af6f9d921ea4d972b47cef26493bb47d0af
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-16 23:07:16 +0300

    CORS

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,9 +1,11 @@
 // Массив доменов, с которых разрешены кросс-доменные запросы
-const allowedCors = [
-  'http://gazievri.mesto.nomoredomains.sbs/',
-  'https://gazievri.mesto.nomoredomains.sbs/',
-  'localhost:3000',
-];
+const allowedCors = {
+  origin: [
+    'http://gazievri.mesto.nomoredomains.sbs/',
+    'https://gazievri.mesto.nomoredomains.sbs/',
+    'localhost:3000',
+  ],
+};
 
 const cors = (req, res, next) => {
   const { origin } = req.headers;
