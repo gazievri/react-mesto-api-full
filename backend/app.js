42ca29c24bfdf780adda8ca9f362b99cb31c93cc
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-16 16:28:10 +0300

    cors 3

--- backend/app.js
+++ backend/app.js
@@ -14,8 +14,6 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.use(cors());
-
 app.use(cookieParser());
 
 app.use(bodyParser.json());
@@ -26,6 +24,8 @@ mongoose.connect('mongodb://127.0.0.1/mestodb', {
 });
 
 app.use(requestLogger); // подключаем логгер запросов
+
+app.use(cors());
 
 app.use(routerUsers);
 app.use(routerCards);
