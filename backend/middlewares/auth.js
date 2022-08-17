56529d747fef24f204a287c4460d5513008fb226
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-17 15:18:13 +0300

    headers

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -8,7 +8,7 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.cookies;
+  const { authorization } = req.headers;
   console.log(authorization);
 
   if (!authorization) {
