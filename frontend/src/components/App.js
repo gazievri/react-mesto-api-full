e3d4aba77ddf93762fc46fb57b8a80073b4402f3
Ruslan Gaziev <grinrus@yandex.ru>
2022-08-17 16:28:03 +0300

    return

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -108,7 +108,7 @@ function App() {
     const isLiked = card.likes.some(i => i._id === currentUser._id);
     api.changeLikeCardStatus(card._id, !isLiked)
     .then((newCard) => {
-      setDataCards((state) => state.map((c) => c._id === card._id ? newCard.data : c));
+      setDataCards((state) => state.map((c) => c._id === card._id ? newCard : c));
     })
     .catch(err => console.log(err));
   }
